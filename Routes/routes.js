d2411464c287ecb19998b5cfeb6d2e2d90b98d23
Senthamizharasu-MS <senthamizharasum.ms2020cse@sece.ac.in>
2022-08-26 11:55:45 +0530

    product post call is completed

--- Routes/routes.js
+++ Routes/routes.js
@@ -1,18 +1,28 @@
 import { productmodel } from "../schema/schema.js";
 
 export const addproductData = (req,res)=>{
-
-    const productData = new productmodel(req.body);
-    console.log(req.body)
-    productData.save((err,data)=>{
-        if(err)
-        {
-            res.send(err)
-        }
-        res.send({
-            status:200,
-            message:"Product data saved !",
-            data
+    if(req.body.productUrl && req.body.productName && req.body.productPrice)
+    {
+        const productData = new productmodel(req.body);
+        console.log(req.body)
+        productData.save((err,data)=>{
+            if(err)
+            {
+                res.send(err)
+            }
+            res.send({
+                status:201,
+                message:"Product data saved !",
+                data:data
+            })
         })
-    })
+    }
+    else
+    {
+        res.send({
+            status:501,
+            message:"Kindly recheck your product"
+        })
+    }
+    
 }
